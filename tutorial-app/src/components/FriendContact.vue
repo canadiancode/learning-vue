361db37ebc296b75699e48a3554fc9e5d19076a4
KevinHeidema <45132144+canadiancode@users.noreply.github.com>
2023-07-03 21:50:21 -0700

    Learning about props and emit

---
+++ tutorial-app/src/components/FriendContact.vue
@@ -0,0 +1,55 @@
+<template>
+    <li>
+        <h2>{{ name }} {{ friendIsFavorite === true ? '(Favorite)' : '' }}</h2>
+        <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }}</button>
+        <button @click="toggleFavorite">Toggle Favorite</button>
+        <ul v-if="detailsAreVisible">
+            <li>Phone: {{ phoneNumber }}</li>
+            <li>Email: {{ emailAddress }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+    export default {
+        // props: [
+        //     'name',
+        //     'phoneNumber',
+        //     'emailAddress',
+        //     'isFavorite',
+        // ],
+        props: {
+            name: {
+                type: String,
+                required: true,
+            },
+            phoneNumber: {
+                type: String,
+                required: true,
+            },
+            emailAddress: {
+                type: String,
+                required: true,  
+            },
+            isFavorite: {
+                type: Boolean,
+                required: false,
+                default: false,
+            },
+        },
+        data() {
+            return {
+                detailsAreVisible: false,
+                friendIsFavorite: this.isFavorite,
+            };
+        },
+        methods: {
+            toggleDetails() {
+                this.detailsAreVisible = !this.detailsAreVisible;
+            },
+            toggleFavorite() {
+                this.$emit('toggle-favorite');
+            }
+        }
+    }
+</script>
---
+++ tutorial-app/src/main.js
@@ -0,0 +1,21 @@
+// import = import/export JavaScript to different files
+// use ./ when importing files, no need for package imports
+
+// Importing createApp method from vue package
+import { createApp } from 'vue';
+
+// import default export from App.vue file (the "App" is just a variable)
+// importing the default export object from the App.vue file
+import App from './App.vue';
+
+// Create Vue app using createApp method
+const app = createApp(App);
+
+// import the FriendContact component file into the main.js file
+import FriendContact from './components/FriendContact.vue';
+
+// add component to Vue app
+app.component('friend-contact', FriendContact);
+
+// Mounting to the public > index.html div with the ID of app
+app.mount('#app');
